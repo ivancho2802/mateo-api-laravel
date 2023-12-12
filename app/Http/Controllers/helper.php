826927a624b19936211c47c64e72b581bc426a30
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2023-12-12 09:26:59 -0500

    asjustes de api configuracion de conexion

---
+++ app/Http/Controllers/helper.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class helper extends Controller
+{
+    //
+    /**
+     * Encode array from latin1 to utf8 recursively
+     * @param $dat
+     * @return array|string
+     */
+    public static function convert_from_latin1_to_utf8_recursively($dat)
+    {
+        if (is_string($dat)) {
+            return mb_convert_encoding($dat, 'UTF-8', 'ISO-8859-2');
+        } elseif (is_array($dat)) {
+            $ret = [];
+            foreach ($dat as $i => $d) $ret[$i] = self::convert_from_latin1_to_utf8_recursively($d);
+            return $ret;
+        } elseif (is_object($dat)) {
+            foreach ($dat as $i => $d) $dat->$i = self::convert_from_latin1_to_utf8_recursively($d);
+            return $dat;
+        } else {
+            return $dat;
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\MFormularios;
 use App\Models\MKoboFormularios;
+use App\Http\Controllers\helper;
+use Illuminate\Http\Response;
+
+//ini_set('internal_encoding', 'utf-8');
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +22,59 @@ use App\Models\MKoboFormularios;
 
 Route::get('/formularios_master', function (Request $request) {
 
-    return MFormularios::get();
+    $body = [
+        "NOMBRES",
+
+        'ID_M_FORMULARIOS',
+        //'email',
+        //'password',
+        "FECHA",
+        "FECHA_REGISTRO",
+        "ACCION",
+        "UNICO",
+        "BARCODE",
+        "ID",
+        "ID_EMPRESA",
+        "CAMPO1",
+        "CAMPO2",
+        "CAMPO3",
+        "CAMPO4",
+        "CAMPO5",
+        "ESTATUS",
+        "VIA",
+        "TIPO",
+        "UID",
+        "URL_DATA",
+        "URL_CAMPOS",
+        /* 
+        */
+        "COMENTARIOS",
+        "GRUPO", 
+        //relacionales
+        "ID_M_CLIENTES",
+        "ID_M_USUARIOS",
+        "ID_M_AREAS",
+    ];
+
+    try {
+        $results = [];
+    
+        $results = MFormularios::select($body)
+        /*
+        ->where(['ID_M_FORMULARIOS'=> '0012'])
+         ->foreach($form=>{
+
+        }) */
+        ->get();
+    
+        //$results = helper::convert_from_latin1_to_utf8_recursively($results);
+        return $results;
+        //return $results[0];//mb_convert_encoding($results[0]['NOMBRES'], 'UTF-8', 'UTF-8');
+        //return response(["message" => "Model status successfully updated!", "data" =>  json_encode($results->toArray())], 200);
+    }catch(\Throwable $exception){
+        return response()->json(['Error' => $exception->getMessage()]);
+    }
+
 });
 
 Route::get('/formularios_kobo_master', function (Request $request) {
