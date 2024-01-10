3a6574a040fa0793f47063a31ecbedffc7c6b0f2
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-01-10 17:06:30 -0500

    creacion de migraciones para postgresql

---
+++ database/migrations/2024_01_10_133759_create_master_lpa_personas_table.php
@@ -0,0 +1,99 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMasterLpaPersonasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('M_LPA_PERSONAS', function (Blueprint $table) {
+            //
+            $table->id();
+            //N. Identificación (number)	OBLIGATORIA
+            $table->string('documento')->unique();
+
+            //Tipo de documento	(string) OBLIGATORIA
+            $table->string('tipo_documento');
+            
+            //Primer Nombre	(string) OBLIGATORIA
+            $table->string('nombre_primero');
+            
+            //Otros Nombres	(string) 
+            $table->string('nombre_otros')->nullable();
+
+            //Primer apellido	(string) OBLIGATORIA
+            $table->string('apellido_primero');
+
+            //Segundo apellido	(string)
+            $table->string('apellido_otros')->nullable();
+
+            //Sexo (H/M)	(string) OBLIGATORIA
+            $table->string('genero');
+
+            //Identidad de género	(string)
+            $table->string('identidad_genero')->nullable();
+            
+            //Fecha de nacimiento	(string) OBLIGATORIA
+            $table->date('fecha_nacimiento');
+            
+            //Nacionalidad	(string) OBLIGATORIA
+            $table->string('nacionalidad');
+            
+            //Perfil Migratorio	(string)
+            $table->string('perfil_migratorio')->nullable();
+            
+            //Situación / Condición	(string) OBLIGATORIA
+            $table->string('situacion');
+            
+            //Pertenencia Étnica	(string) OBLIGATORIA
+            $table->string('etnia');
+
+            //Perfil del Participante	(string) OBLIGATORIA
+            $table->string('perfil');
+            
+            //Nivel de escolaridad	(string) 
+            $table->string('nivel_escolaridad')->nullable();
+
+            //Caracteristica Madre	(string) OBLIGATORIA
+            $table->string('caracteristica_madre');
+
+            //Situación de Discapacidad OBLIGATORIA
+                //Dificultad para ver	(string) 
+                //Dificultad para oir	(string)
+                //Dificultad para caminar	(string)
+                //Dificultad para recordar	(string)
+                //Dificultad para el cuidado propio	(string)
+                //Dificultad para comunicar	(string)
+            $table->string('discapacidad_ver')->nullable();
+            $table->string('discapacidad_oir')->nullable();
+            $table->string('discapacidad_caminar')->nullable();
+            $table->string('discapacidad_recordar')->nullable();
+            $table->string('discapacidad_cuidadopropio')->nullable();
+            $table->string('discapacidad_comunicar')->nullable();
+            
+            //Celular	(numero)
+            $table->tinyInteger('telefono')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('M_LPA_PERSONA', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ database/migrations/2024_01_10_145802_create_master_lpas_table.php
@@ -0,0 +1,88 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMasterLpasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('M_LPAS', function (Blueprint $table) {
+            //
+            $table->id();
+            $table->unsignedBigInteger('fk_lpa_emergencia');
+            $table->unsignedBigInteger('fk_lpa_persona');
+            $table->timestamps();
+
+            $table->foreign('fk_lpa_emergencia')
+            ->nullable()
+            ->references('id')->on('M_LPA_EMERGENCIAS');
+
+            
+            $table->foreign('fk_lpa_persona')
+            ->nullable()
+            ->references('id')->on('M_LPA_PERSONAS');
+
+            //----------------DATOS DEL SERVICIO
+
+            //Donante	(string) OBLIGATORIA
+            $table->string('donante');
+
+            //Código de Actividad (string) OBLIGATORIA	
+            $table->string('cod_actividad');
+
+            //Fecha de atención	(string) OBLIGATORIA
+            $table->date('fecha_atencion');
+
+            //Representante del Hogar o Beneficiario Directo (string)	
+            $table->string('representante')->nullable();
+            //ID del Hogar o Beneficiario Directo	(string)
+            $table->string('doc_representante')->nullable();
+            //----------------DATOS DEL SERVICIO END
+
+
+            //----------------DATOS COMPLEMENTARIO KIT WATCH
+            //Tipo de transferencia	(string)
+            $table->string('tipo_tranferencia')->nullable();
+            
+            //Mecanismo de Entrega	-(string)
+            $table->string('modo_entrega')->nullable();
+            
+            //Proveedor Financiero	(string)
+            $table->string('proveedor_financiero')->nullable();
+            
+            //Monto que recibe en el mes en COP   (string)	
+            $table->string('monto_mensual')->nullable();
+            //----------------DATOS COMPLEMENTARIO KIT WATCH END
+
+            
+            //----------------naranja "datos de validacion de formularios
+            //Edad	
+            //"Rango ECHO"	
+            //"Rango BHA"	
+            //Status	
+            //Unicos	
+            //Validación				
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('M_LPA', function (Blueprint $table) {
+            //
+        });
+    }
+}
