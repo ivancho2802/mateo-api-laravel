52d5ab352925252f0e675f75beab6d76bb458987
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-02-14 10:52:45 -0500

    excluir uso de csf token para login

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,7 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        '/login',
-        '/register'
+        //'/login',
+        //'/register'
     ];
 }
