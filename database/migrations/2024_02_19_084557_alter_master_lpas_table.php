88f129277cad68439d247b6afacf5086885619f5
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-02-19 09:41:30 -0500

    validacion migracion de referencioa foreign key actividades lpa

---
+++ database/migrations/2024_02_19_084557_alter_master_lpas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterMasterLpasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('"M_LPAS"', function (Blueprint $table) {
+            $table->string('COD_ACTIVIDAD')->unsigned()->change();
+            $table->foreign('COD_ACTIVIDAD')
+                ->references('cod')->on('activities')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
