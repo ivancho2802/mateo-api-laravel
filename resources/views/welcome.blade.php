cc729ecc34d95d2160b5e9d9eb372b37af5d1dd4
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-04-05 15:53:09 -0500

    se documenta el servicio para generar qr

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -482,11 +482,14 @@
 
           <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
             <div class="flex items-center">
-              <img src="https://mireview.ach.dyndns.info/ach/imagenes/inicio/infoMIRE3.jpg" class="w-8 h-8 text-gray-500" />
-              <!-- <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg> -->
-              <div class="ml-4 text-lg leading-7 font-semibold"><a href="/urls" class="underline text-gray-900 dark:text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                <path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
+              </svg>
+              <div class="ml-4 text-lg leading-7 font-semibold">
+                <a href="/urls" class="underline text-gray-900 dark:text-white">
                   Acortar link
-                </a></div>
+                </a>
+              </div>
             </div>
 
             <div class="ml-12">
@@ -511,7 +514,7 @@
                 Formato url para importar en excel o en powe bi
                 <br>
 
-                http://ach.dyndns.info:6180/api/kobo/[asset_uid]/data/[token]
+                https://mireview.api.ach.dyndns.info/api/kobo/[asset_uid]/data/[token]
                 <br>
 
                 [token] = es el usado por kobo para obtrener datos
@@ -532,11 +535,14 @@
 
           <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
             <div class="flex items-center">
-              <img src="https://mireview.ach.dyndns.info/ach/imagenes/inicio/infoMIRE3.jpg" class="w-8 h-8 text-gray-500" />
-              <!-- <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg> -->
-              <div class="ml-4 text-lg leading-7 font-semibold"><a href="/urls" class="underline text-gray-900 dark:text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                <path d="M64 256V160H224v96H64zm0 64H224v96H64V320zm224 96V320H448v96H288zM448 256H288V160H448v96zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z" />
+              </svg>
+              <div class="ml-4 text-lg leading-7 font-semibold">
+                <a href="/urls" class="underline text-gray-900 dark:text-white">
                   matriz minas
-                </a></div>
+                </a>
+              </div>
             </div>
 
             <div class="ml-12">
@@ -544,41 +550,31 @@
                 Esta herramienta funciona para obtener los datos de una matriz de prensa (esta consiste en extraer y contar palabras dentro de un conjunto de datos establecidos para tener datos medibless ).
                 <br>
 
-                http://ach.dyndns.info:6180/api/matriz/minas
+                https://mireview.api.ach.dyndns.info/api/matriz/minas
 
               </div>
             </div>
           </div>
 
-          <!-- <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
-                        <div class="flex items-center">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                                <path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                            </svg>
-                            <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Servicio para obtener datos de un formulario kobo con labes</div>
-                        </div>
+          <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
+            <div class="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 80C0 53.5 21.5 32 48 32h96c26.5 0 48 21.5 48 48v96c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V80zM64 96v64h64V96H64zM0 336c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v96c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V336zm64 16v64h64V352H64zM304 32h96c26.5 0 48 21.5 48 48v96c0 26.5-21.5 48-48 48H304c-26.5 0-48-21.5-48-48V80c0-26.5 21.5-48 48-48zm80 64H320v64h64V96zM256 304c0-8.8 7.2-16 16-16h64c8.8 0 16 7.2 16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s7.2-16 16-16s16 7.2 16 16v96c0 8.8-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s-7.2-16-16-16s-16 7.2-16 16v64c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V304zM368 480a16 16 0 1 1 0-32 16 16 0 1 1 0 32zm64 0a16 16 0 1 1 0-32 16 16 0 1 1 0 32z"/></svg>
+              <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">
+                Servicio para crear qr con la marca de ach  
+              </div>
+            </div>
 
-                        <div class="ml-12">
-                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                Esta herramienta funciona para generar la misma informacion de un kobo pero con sus header o titulos de columnas de una mejro forma como se ve en los labes 
-                                <br>
-                                Formato url para importar en excel o en powe bi
-                                <br>
-
-                                http://ach.dyndns.info:6180/api/kobo/[asset_uid]/data/[token]
-                                <br>
-
-                                [token] = es el usado por kobo para obtrener datos
-                                <br>
-
-                                [asset_uid] = hash del formulario o id del formulario de kobo
-                                <br>
-
-                                ademas de poner el token en la url debes poner el token de la api MIREsys para acceder a los datos
-                                <br>
-                            </div>
-                        </div>
-                    </div> -->
+            <div class="ml-12">
+              <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                Esta herramienta funciona para generar qr en base a links o texto o demas funciones que quieras usar
+                <br>
+                https://mireview.api.ach.dyndns.info/qr?url=%22https://mireview.api.ach.dyndns.info/cut/XujVh%22
+                <br>
+                [url] = podria ser una url o un texto correo o demas
+                <br>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
