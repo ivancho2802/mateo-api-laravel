b7740d4b93dc47b8f663533966051123f6879f7e
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-04-05 15:01:30 -0500

    se ajusta para que se vea mejor las imagens del inio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -429,7 +429,7 @@
             @endif
 
             <div class="flex justify-center pt-8 sm:justify-center sm:pt-0 bg-white ">
-                <img src="http://ach.dyndns.info:6080/webMIRE/images/Bandera_Logos_MIRE.png" class="h-16 w-auto text-gray-700 sm:h-20" />
+                <img src="https://mireview.ach.dyndns.info/ach/imagenes/inicio/Bandera_Logos_MIRE.png" class="h-16 w-auto text-gray-700 sm:h-20" />
 
                 <!-- <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto text-gray-700 sm:h-20">
                         <g clip-path="url(#clip0)" fill="#EF3B2D">
@@ -446,7 +446,7 @@
                 <div class="grid grid-cols-1 md:grid-cols-2">
                     <div class="p-6">
                         <div class="flex items-center">
-                            <img src="http://ach.dyndns.info:6080/webMIRE/images/MIREsys4.jpg" class="w-8 h-8 text-gray-500" />
+                            <img src="https://mireview.ach.dyndns.info/ach/imagenes/inicio/MIREsys4.jpg" class="w-8 h-8 text-gray-500" />
                             <!-- <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg> -->
                             <div class="ml-4 text-lg leading-7 font-semibold"><a href="http://ach.dyndns.info:8888/ach/herramientas/password/password.php?db=ach" class="underline text-gray-900 dark:text-white">
                                     MIREsys
@@ -462,7 +462,7 @@
 
                     <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
                         <div class="flex items-center">
-                            <img src="http://ach.dyndns.info:6080/webMIRE/images/infoMIRE3.jpg" class="w-8 h-8 text-gray-500" />
+                            <img src="https://mireview.ach.dyndns.info/ach/imagenes/inicio/infoMIRE3.jpg" class="w-8 h-8 text-gray-500" />
                             <!-- <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg> -->
                             <div class="ml-4 text-lg leading-7 font-semibold"><a href="http://ach.dyndns.info:8888/ach/movil_dashboard/www/index.html" class="underline text-gray-900 dark:text-white">
                                     infoMIRE
@@ -482,7 +482,7 @@
 
                     <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
                         <div class="flex items-center">
-                            <img src="http://ach.dyndns.info:6080/webMIRE/images/infoMIRE3.jpg" class="w-8 h-8 text-gray-500" />
+                            <img src="https://mireview.ach.dyndns.info/ach/imagenes/inicio/infoMIRE3.jpg" class="w-8 h-8 text-gray-500" />
                             <!-- <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg> -->
                             <div class="ml-4 text-lg leading-7 font-semibold"><a href="/urls" class="underline text-gray-900 dark:text-white">
                                     Acortar link
@@ -532,7 +532,7 @@
 
                     <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
                         <div class="flex items-center">
-                            <img src="http://ach.dyndns.info:6080/webMIRE/images/infoMIRE3.jpg" class="w-8 h-8 text-gray-500" />
+                            <img src="https://mireview.ach.dyndns.info/ach/imagenes/inicio/infoMIRE3.jpg" class="w-8 h-8 text-gray-500" />
                             <!-- <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg> -->
                             <div class="ml-4 text-lg leading-7 font-semibold"><a href="/urls" class="underline text-gray-900 dark:text-white">
                                 matriz minas
