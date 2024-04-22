6c4b59174cc2b79986dbfd3e954cfa0133aec345
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-04-22 17:06:38 -0500

    se jausta ára ver por que no funciona mps

--- app/Http/Controllers/Meal.php
+++ app/Http/Controllers/Meal.php
@@ -698,7 +698,7 @@ class Meal extends Controller
             $q->where('ACCION', '=', "MPD");
         })
         ->get()
-        ->with('pregunta')
+        ->with(['pregunta'])
         ->groupBy('_ID');
 
         dd(count($mmpds));
