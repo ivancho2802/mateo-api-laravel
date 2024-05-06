cef64c07f397e7c4f74d25ba962a3399d75a3bed
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-05-06 09:43:28 -0500

    se ajusta la web

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
 
         <title>{{ config('app.name', 'Laravel') }}</title>
@@ -10,7 +11,7 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
+        <!-- Styles ... -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <!-- Scripts -->
