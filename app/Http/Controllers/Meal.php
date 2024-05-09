35ea824e15822c3456ecd74bac07d10700c25523
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-05-09 14:36:58 -0500

    se agrega  las variacio de los elemenos del formulario pdm para que salgan ceros y 1 para

--- app/Http/Controllers/Meal.php
+++ app/Http/Controllers/Meal.php
@@ -55,10 +55,11 @@ class Meal extends Controller
         //"Rango BHA"
         //=SI(AM2="";"";SI(AM2<=4;"0 to 4";SI(AM2<=9;"5 to 9";SI(AM2<=14;"10 to 14";SI(AM2<=18;"15 to 18";SI(AM2<=29;"19 to 29";SI(AM2<=59;"30 to 59";SI(AM2>=60;"> 60"))))))))
 
-        $mlpas->load(['emergencia', 'actividad', 'persona', 'actividad.directory']);
+        $mlpas->load(['emergencia', 'actividad', 'persona']);
   
 
         $mlpasFormated = $mlpas->map(function ( $lpa) {
+            $lpa->load('actividad.directory');
             $lpa->append('tipo_lpa');
             $lpa->persona->append('edad');
             $lpaArray = $lpa->toArray();
