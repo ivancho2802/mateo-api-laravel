f5b40531892e5a5e0077f44afffee647b3f0b260
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-05-16 14:21:16 -0500

    ajustar render docekrfile

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
-#composer update
 composer install --no-dev --working-dir=/var/www/html
-php artisan key:generate
-php artisan storage:link
 
 echo "Caching config..."
 php artisan config:cache
@@ -13,4 +10,4 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate:refresh --force
+php artisan migrate --force
