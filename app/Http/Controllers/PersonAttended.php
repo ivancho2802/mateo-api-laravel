cdf6d88957d202e07ce207fb7de57f9a928f6271
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-05-16 08:10:26 -0500

    se ajusta para que en la migracion el estatus funcione

--- app/Http/Controllers/PersonAttended.php
+++ app/Http/Controllers/PersonAttended.php
@@ -258,7 +258,7 @@ class PersonAttended extends Controller
         //try {
 
 
-            $lotes = 50;
+            $lotes = 40;
 
             $ID_USER = Auth::user()->id ?? optional(Auth::user())->ID;
 
