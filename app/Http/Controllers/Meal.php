567d3a3356b4b2139f0dee4cde9a91ea9f78effa
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-05-28 16:28:11 -0500

    se ajusta lpa para que cargue mas rapido

--- app/Http/Controllers/Meal.php
+++ app/Http/Controllers/Meal.php
@@ -47,7 +47,7 @@ class Meal extends Controller
             return $mlpas;
         } else {
             
-            $mlpas = MLpa::limit(10000)->nodeleted()->get(); //where("FECHA_ATENCION", ">=", "2024-01-01")limit(60000)->
+            $mlpas = MLpa::nodeleted()->get(); //where("FECHA_ATENCION", ">=", "2024-01-01")limit(60000)->
         }
 
         //PONER LA PERSONA CON SU EDAD
