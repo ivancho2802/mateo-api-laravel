45f99766bb5f052c44c72edf8d353ab56121266d
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-06-06 17:20:10 -0500

    se ajusta para que se pyueda actualizar mas rapida las lpa

--- aux_online.sql
+++ aux_online.sql
@@ -19,8 +19,39 @@ SELECT COUNT(*) FROM (SELECT DISTINCT "DOCUMENTO", "TIPO_DOCUMENTO" FROM "M_LPA_
 select "ID_M_USUARIOS" from "M_LPAS" LIMIT 10
 select "DOCUMENTO", "TIPO_DOCUMENTO"  from "M_LPA_PERSONAS" order by "DOCUMENTO"
 
-select * FROM "M_LPAS" where "FECHA_ATENCION" >= '2024-02-01'
-delete FROM "M_LPAS" where "FECHA_ATENCION" < '2024-02-01'
+select * FROM "M_LPAS" where "FECHA_ATENCION" < '2000-02-01'
 select * from migrate_customs where "table" = 'M_LPAS' and file_ref = 'UPLOADED' limit 10
 
 select * from migrate_customs where "table" = 'M_LPAS' and table_id like '%migrationsLpa%'  order by updated_at desc limit 20
+
+select * from matrizs where origin = 'matriz_matriz oleoductos';
+
+-- tipos de respuestas para doiscapacidades de persoinas
+
+select "DISCAPACIDAD_COMUNICAR" from "M_LPA_PERSONAS" GROUP BY "DISCAPACIDAD_COMUNICAR"
+
+select "DISCAPACIDAD_VER", "ID" from "M_LPA_PERSONAS" where "DISCAPACIDAD_VER" = 'Si - No puede hacerlo' 
+
+select *  from analisis
+
+select "CHANNEL_IN" from "M_MQR" GROUP BY "CHANNEL_IN"
+
+select "TIPO_EVENTO" FROM "M_LPA_EMERGENCIAS" GROUP BY "TIPO_EVENTO"
+
+select "ORG_REPORT" from "M_MQR" GROUP BY "ORG_REPORT"
+
+update analisis set texto = 'No hay datos' where texto is null;
+update analisis set acceso = 'No hay datos' where acceso is null;
+update analisis set participacion = 'No hay datos' where participacion is null;
+update analisis set ajustes = 'No hay datos' where ajustes is null;
+update analisis set respuesta_rapida = 'No hay datos' where respuesta_rapida is null;
+update analisis set recuperacion_temprana = 'No hay datos' where recuperacion_temprana is null;
+update analisis set acompanamiento = 'No hay datos' where acompanamiento is null;
+select *  from analisis;
+
+
+
+
+
+
+
