52725020f274e9b12c259848eb6888f0d87dee1f
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-06-09 09:40:43 -0500

    se ajusta para que donantes salga le grupo

--- app/Http/Controllers/Meal.php
+++ app/Http/Controllers/Meal.php
@@ -90,7 +90,12 @@ class Meal extends Controller
         ini_set('max_input_time', '' . $limit_minutes . '');
         
         $mlpas = MLpa::where("FECHA_ATENCION", ">=", "2023-01-01")->nodeleted()->get(); //where("FECHA_ATENCION", ">=", "2024-01-01")limit(60000)->limit(20000)->
+
         $donantes = $mlpas->groupBy('DONANTE')->keys();
+        
+        if(optional($request)->donante){
+            $mlpas->where("DONANTE", "=", $request->donante);
+        }
 
         return [
             "lpas" => [
