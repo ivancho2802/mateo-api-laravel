b7851a447af7833db429df8a4719abdcf90e2702
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-06-12 14:39:22 -0500

    se ajustan algunos cambios para que se almacenen algunos d¿atos de la matriz

--- app/Http/Controllers/Kobo.php
+++ app/Http/Controllers/Kobo.php
@@ -911,6 +911,8 @@ class Kobo extends Controller
             return null;
         }
 
+        dd($formulariokobo_region);
+
         $cod_region_str = $formulariokobo_region[0]->VALOR;
 
         $cod_region = $this->getCodeKoboByRegion($cod_region_str, $rtrecords_kobo);
