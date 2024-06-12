ca3e85b3e9ebfc6e94030378f807b54e56acbf6f
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-06-12 11:08:08 -0500

    se ajustan algunos cambios para que se almacenen algunos d¿atos de la matriz

--- app/Http/Controllers/Kobo.php
+++ app/Http/Controllers/Kobo.php
@@ -846,5 +846,15 @@ class Kobo extends Controller
             "rt_mireview_firebird" => $resultados,
         ];
 
+
+        $dataSubdmissions->each(function ($kobo_rt) {
+            $xcodigo_alerta_str = $kobo_rt['identificacion/Corregimiento_consejo_vereda'];
+            if(!strpos($xcodigo_alerta_str, '-')){
+                $xcodigo_alerta = explode('-', $xcodigo_alerta_str)[1];
+
+                dd($xcodigo_alerta);
+            }
+
+        });
     } 
 }
