7a6882633b1b19c2285892dbd8812a6f9b84d3d5
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-07-01 15:53:34 -0500

    se crea un boton para descargar zip con los pdf si las descargas estan listas

--- resources/views/koboapdf/index.blade.php
+++ resources/views/koboapdf/index.blade.php
@@ -265,7 +265,7 @@
                                 </svg>
                               </a>
                             </td>
-                            @elseif
+                            @else
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="width: 50px;fill: red;"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"/></svg>
                             @endif
 
@@ -345,6 +345,8 @@
                                 </svg>
                               </a>
                             </td>
+                            @else
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="width: 50px;fill: red;"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"/></svg>
                             @endif
                           </tr>
                           @empty
