106770fb82b789015c2650442b925dcd83e8f5dd
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-07-16 08:08:22 -0500

    ajuste de menu de

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -640,7 +640,7 @@
 
               <div class="ml-12">
                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                  Esta herramienta funciona para obtener realizar descargas masivas de los formulario de un kobo en especifico es decir todas las respuestas en un formato especifico se tiene una plantilla 
+                  Esta herramienta funciona para obtener realizar descargas masivas de los formulario de un kobo en especifico es decir todas las respuestas en un formato especifico se tiene una plantilla
                   en base a un formulario basico es posible que no funcione con todos los formularios si esto ocurre debes realizar la solicitud
                   <br>
 
@@ -653,35 +653,33 @@
 
           </div>
 
+          <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
+            <div class="flex items-center">
+              <i class="ach-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                  <path d="M64 256V160H224v96H64zm0 64H224v96H64V320zm224 96V320H448v96H288zM448 256H288V160H448v96zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z" />
+                </svg>
+              </i>
+
+              <div class="ml-4 text-lg leading-7 font-semibold">
+                <a href="/finanzas/adn" class="underline text-gray-900 dark:text-white">
+                  Parametros de finanzas adn fase 2
+                </a>
+              </div>
+            </div>
+
+            <div class="ml-12">
+              <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                Esta herramienta es para adn.
+                <br>
+
+                https://ugic.api.ach.dyndns.info/finanzas/adn
+
+              </div>
+            </div>
+          </div>
+
         </div>
-      </div>
-
-      <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
-
-        <!-- <div class="text-center text-sm text-gray-500 sm:text-left">
-          <div class="flex items-center">
-            <i class="ach-icon">
-              <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
-                <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-              </svg>
-            </i>
-
-
-            <a href="https://laravel.bigcartel.com" class="ml-1 underline">
-              Shop
-            </a>
-
-            <i class="ach-icon">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="ml-4 -mt-px w-5 h-5 text-gray-400">
-                <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-              </svg>
-            </i>
-
-
-            <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-              Sponsor
-            </a>
-          </div> -->
       </div>
 
       <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
