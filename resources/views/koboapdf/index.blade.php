91dd0ea4e6e104aeef4d6ff8150c717ed7911cb5
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-07-25 09:32:24 -0500

    se ajusta para que pueda enviar parametros

--- resources/views/koboapdf/index.blade.php
+++ resources/views/koboapdf/index.blade.php
@@ -292,8 +292,8 @@
                       </div>
                       @endif
 
-                      <h4>Registros enviados: {{count($data)}}</h4>
                       @if(isset($data))
+                      <h4>Registros enviados: {{count($data)}}</h4>
 
                       <ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
                         @forelse ($data as $export)
