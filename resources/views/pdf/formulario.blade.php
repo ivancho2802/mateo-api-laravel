cb224e748f293a4b6a708e80179fd613eac9c349
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-07-30 11:56:45 -0500

    se ajusta para que se pueda descargar los datos desde api

--- resources/views/pdf/formulario.blade.php
+++ resources/views/pdf/formulario.blade.php
@@ -109,7 +109,7 @@
       }
     }
   </style>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://ugic.api.ach.dyndns.info/public/download/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
   <link rel="stylesheet" media="all" type="text/css" href="https://ee.acf-e.org/css/theme-grid.print.css">
   <link rel="stylesheet" media="all" type="text/css" href="https://mireview.api.ach.dyndns.info/theme-grid.css">
 
