d086bf999db58acb035b21c2b0691bcc10a2aa21
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-08-22 10:55:17 -0500

    se crea lo necesario para poder realziar pprocesos de josb para completar las migracionde delpa ppor pasos

--- app/Models/MLpaMongo.php
+++ app/Models/MLpaMongo.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use MongoDB\Laravel\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\DocumentModel;
+use MongoDB\Laravel\Relations\EmbedsMany;
 
 class MLpaMongo extends Model
 {
