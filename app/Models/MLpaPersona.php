567ffd5d19c4ff4637c23923843f3f4e98e974aa
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-08-27 16:18:34 -0500

    se ajusta para que salga el donante defiitivo

--- app/Models/MLpaPersona.php
+++ app/Models/MLpaPersona.php
@@ -21,7 +21,7 @@ class MLpaPersona extends Model
      * @var array
      */
     protected $hidden = [
-        'ID','DOCUMENTO', 'NOMBRE_PRIMERO', 'NOMBRE_OTROS', 'APELLIDO_PRIMERO', 'APELLIDO_OTRO', 'TELEFONO', 'GENERO'
+        'ID','DOCUMENTO', 'NOMBRE_PRIMERO', 'NOMBRE_OTROS', 'APELLIDO_PRIMERO', 'APELLIDO_OTRO', 'TELEFONO'
     ];
 
     public $incrementing = false;
