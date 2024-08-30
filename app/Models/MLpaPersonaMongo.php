11e0b445cc29eb1afa5949b7e9a0eecb17abc959
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-08-30 13:53:38 -0500

    se ajusta para ver mongo clas como se obtienen los datos del archivo tal vez no se ven

--- app/Models/MLpaPersonaMongo.php
+++ app/Models/MLpaPersonaMongo.php
@@ -14,4 +14,37 @@ class MLpaPersonaMongo extends Model
     protected $hidden = [
         'ID','DOCUMENTO', 'NOMBRE_PRIMERO', 'NOMBRE_OTROS', 'APELLIDO_PRIMERO', 'APELLIDO_OTRO', 'TELEFONO', 'GENERO'
     ];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+
+        "DOCUMENTO",
+        "TIPO_DOCUMENTO",
+        "NOMBRE_PRIMERO",
+        "NOMBRE_OTROS",
+        "APELLIDO_PRIMERO",
+        "APELLIDO_OTRO",
+        "GENERO",
+        "IDENTIDAD_GENERO",
+        "FECHA_NACIMIENTO",
+        "NACIONALIDAD",
+        "PERFIL_MIGRATORIO",
+        "SITUACION",
+        "ETNIA",
+        "PERFIL",
+        "NIVEL_ESCOLARIDAD",
+        "CARACTERISTICAS_MADRE",
+        'DISCAPACIDAD_VER',
+        'DISCAPACIDAD_OIR',
+        'DISCAPACIDAD_CAMINAR',
+        'DISCAPACIDAD_RECORDAR',
+        'DISCAPACIDAD_CUIDADO_PROPIO',
+        'DISCAPACIDAD_COMUNICAR',
+        "TELEFONO"
+
+    ];
+
 }
