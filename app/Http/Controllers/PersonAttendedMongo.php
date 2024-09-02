8d671c3cd5e689ff9fc14dc083ae13b1dc1be861
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-09-02 16:53:05 -0500

    se crea un servicio para pdoer eliminar lpas

--- app/Http/Controllers/PersonAttendedMongo.php
+++ app/Http/Controllers/PersonAttendedMongo.php
@@ -482,7 +482,7 @@ class PersonAttendedMongo extends Controller
             return ["no borro nada"];
         }
 
-        $response = MLpaMongo::remove();
+        $response = MLpaMongo::deleteMany();
 
         return [$response];
 
