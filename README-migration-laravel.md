50b5304bb436e54b1b77dc10c2287af239ff37b4
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-09-12 16:56:21 -0500

    se ajustan para que se vean productos de informacion en recuperacion temprana

--- README-migration-laravel.md
+++ README-migration-laravel.md
@@ -8,7 +8,7 @@
 # migrar archiv especifico
 php artisan migrate --path=/database/migrations/test/
 php artisan migrate:refresh --path=/database/migrations/2024_03_13_170435_alter_lpa_table.php
-php artisan migrate:refresh --path=/database/migrations/2024_05_27_094538_alter_matrizs_table.php
+php artisan migrate:refresh --path=/database/migrations/2024_07_26_083931_alter_adns_table.php
 
 
 # crar un modelo con su controlador
--- database/migrations/2024_07_26_083931_alter_adns_table.php
+++ database/migrations/2024_07_26_083931_alter_adns_table.php
@@ -15,11 +15,11 @@ class AlterAdnsTable extends Migration
     {
         //
         Schema::table('adns', function (Blueprint $table) {
-            $table->string('dinero_ahorrado_x_persona')->nullable()->change();
+            $table->string('dinero_ahorrado_x_persona')->nullable()->create();
             $table->string('grupo_ahorro')->nullable()->change();
             $table->string('personas_bancarizadas')->nullable()->change();
             $table->string('tasa_cambio')->nullable()->create();
-            
+
         });
     }
 
