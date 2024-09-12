7f45eec8b10420604fd84fa964c0ac38f288e241
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-09-12 16:58:41 -0500

    se ajustan para que se vean productos de informacion en recuperacion temprana

--- database/migrations/2024_07_26_083931_alter_adns_table.php
+++ database/migrations/2024_07_26_083931_alter_adns_table.php
@@ -17,7 +17,7 @@ class AlterAdnsTable extends Migration
         Schema::table('adns', function (Blueprint $table) {
             $table->string('dinero_ahorrado_x_persona')->nullable()->create();
             $table->string('grupo_ahorro')->nullable()->create();
-            $table->string('personas_bancarizadas')->nullable()->change();
+            $table->string('personas_bancarizadas')->nullable()->create();
             $table->string('tasa_cambio')->nullable()->create();
 
         });
