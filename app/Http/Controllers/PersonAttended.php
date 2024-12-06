789ff9189952024c0f23e5a1f570f47807258036
Ivan Orlando Diaz <ivancho2802@hotmail.com>
2024-12-06 14:27:00 -0500

    se ajusta tg crea el servicio para poder recivbir la data para lpa del consorcio para la pgina del consorcio

--- app/Http/Controllers/PersonAttended.php
+++ app/Http/Controllers/PersonAttended.php
@@ -953,8 +953,8 @@ class PersonAttended extends Controller
 
         $mlpas = MLpa::where("FECHA_ATENCION", ">=", "2023-01-01")
             ->with("emergencia")
-            ->nodeleted();
-            //->get(); //where("FECHA_ATENCION", ">=", "2023-01-01")limit(60000)->
+            ->nodeleted()
+            ->get(); //where("FECHA_ATENCION", ">=", "2023-01-01")limit(60000)->
         //->groupBy('FECHA_ATENCION');
 
         //total de personas
