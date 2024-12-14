95b423929373bee10d59423c8aec4e8e052bb43d
Ivan Orlando Diaz <ivancho2802@hotmail.com>
2024-12-14 15:42:09 -0500

    se crea un servicio para la carga de documentos de prod info rr

--- app/Http/Controllers/MqrCaminoMireClass.php
+++ app/Http/Controllers/MqrCaminoMireClass.php
@@ -43,13 +43,40 @@ class MqrCaminoMireClass implements ToCollection
             $DATE_IN = $date_in; //date('d-m-Y', strtotime($date_birday));
             
             $mrq = MqrCaminos::create([
-                'date_entry' => $DATE_IN,
-                'org_report' => $row[1],
-                'type_act' => $row[2],
-                'departamento' => $row[3],
-                'municipio' => $row[4],
-                'women' => $row[5],
-                'men' => $row[6],
+                'fecha' => $DATE_IN,
+                'mes' => $row[1],
+                'organizacion' => $row[2],
+                'tipo_intervencion' => $row[3] ,
+                'codigo' => $row[4],
+                'departamento' => $row[5],
+                'municipio' => $row[6],
+                'comunidad' => $row[7],
+                'ninas' => $row[8],
+                'ninos' => $row[9],
+                'mujeres' => $row[10],
+                'hombres' => $row[11],
+                'hallazgos_programaticos' => $row[12],
+                'recomendaciones_programaticas' => $row[13],
+                'primer_contacto_alegría' => $row[14],
+                'primer_contacto_aburrimiento' => $row[15],
+                'primer_contacto_tristeza' => $row[16],
+                'primer_contacto_enojo' => $row[17],
+                'primer_contacto_NS_NR' => $row[18],
+                'presentacion_MIRE_alegría' => $row[19],
+                'presentacion_MIRE_aburrimiento' => $row[20],
+                'presentacion_MIRE' => $row[21],
+                'enojo' => $row[22],
+                'NS_NR' => $row[23],
+                'alegría' => $row[24],
+                'aburrimiento' => $row[25],
+                'presentacion_MIRE_tristeza' => $row[26],
+                'presentacion_MIRE_enojo' => $row[27],
+                'presentacion_MIRE_NS_NR' => $row[28],
+                'finalizacion_atencion_alegría' => $row[29],
+                'finalizacion_atencion_aburrimiento' => $row[30],
+                'finalizacion_atencion_tristeza' => $row[31],
+                'finalizacion_atencion_enojo' => $row[32],
+                'finalizacion_atencion_NS_NR' => $row[33]
             ]);
 
             $mrq = MqrCaminos::get()->last();
@@ -59,7 +86,7 @@ class MqrCaminoMireClass implements ToCollection
         }
 
         migrateCustom::create([
-            'table' => 'mqr_spaces',
+            'table' => 'mqr_caminos',
             'table_id' => implode(", ", $id_mqr),
             'file_ref' => '-',
         ]);
