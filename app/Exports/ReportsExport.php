9b33bfa5bbc3f65e2518c295856caf14f85a9cea
Ivan Orlando Diaz <ivancho2802@hotmail.com>
2025-01-28 09:31:10 -0500

    se ajusta para poder exportar excel con productos de infromacion

--- app/Exports/ReportsExport.php
+++ app/Exports/ReportsExport.php
@@ -7,13 +7,40 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 
 class ReportsExport implements FromCollection
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
-    {
-        dd("ReportsExport", Reports::all());
+  /**
+   * @return \Illuminate\Support\Collection
+   */
+  public function collection()
+  {
+    //dd("ReportsExport", Reports::all());
 
-        return Reports::all();
-    }
+    $reports = Reports::all();
+
+    $reports_keys = $reports->first();
+
+    $reports_keys_new = $reports_keys->map(function ($report, $key) {
+      $report[$key] = $key;
+      return $report;
+    });
+
+    dd("excel report", $reports_keys, $reports_keys_new);
+
+    $reports->push([
+      "id" => 35,
+      "created_at" => "2024-06-17 13:41:55",
+      "updated_at" => "2024-06-17 13:41:55",
+      "year" => "2024",
+      "codigo_emergencia" => "BONO-981.1",
+      "departamento" => "Bolívar",
+      "municipio" => "Norosí",
+      "tipo_emergencia" => "Desplazamiento masivo",
+      "fecha_ern" => "2024-02-15 00:00:00",
+      "links" => "https://reliefweb.int/report/colombia/evaluacion-rapida-de-necesidades-ern-norosi-bolivar-15022024,https://reliefweb.int/report/colombia/colombia-ficha-de-cierre-de-emergencias-fce-norosi-bolivar-05032024",
+      "ID_M_USUARIOS" => 1,
+      "tipo_respuesta" => "Respuesta Rapida",
+      "tipo_producto" => "ERN,FCE",
+    ]);
+
+    return $reports;
+  }
 }
