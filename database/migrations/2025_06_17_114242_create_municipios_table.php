4ea41f22b4f1abbeb943161a0786186017f78b77
ivan diaz <iodiaz@co.acfspain.org>
2025-06-17 11:44:10 -0500

    se ajusta oara wue salga la lsita de departamentos y municipios

---
+++ database/migrations/2025_06_17_114242_create_municipios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMunicipiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('municipios', function (Blueprint $table) {
+            //
+            $table->id();
+            $table->timestamps();
+            $table->string('name')->nullable();
+            $table->string('fk_departments')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('municipios', function (Blueprint $table) {
+            //
+        });
+    }
+}
