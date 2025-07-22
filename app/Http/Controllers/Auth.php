6d9def256835ee21d68f9ba7c6cc6a57049359ed
ivancho2802 <ivancho2802@hotmail.com>
2025-07-21 21:07:34 -0500

    asd

--- app/Http/Controllers/Auth.php
+++ app/Http/Controllers/Auth.php
@@ -88,13 +88,12 @@ class Auth extends Controller
 
         //Search for the user where the customer is
         $user = User::where('email', $request->email)->first();
-        dd($user);
+
         if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'email' => ['Las credenciales son incorrectas.', "clave"],//strtoupper(md5(strtoupper($request->password))) == $userMire->CLAVE
             ]);
         }
-
 
         $token = $user->createToken($request->device_name . $request->email . $request->password);
         //$csrf_token = csrf_token();
