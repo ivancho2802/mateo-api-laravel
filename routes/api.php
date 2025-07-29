08154500f23f85e9c4efa824d5956b46d1a9a917
ivan diaz <iodiaz@co.acfspain.org>
2025-07-29 07:44:38 -0500

    se ajusta

--- routes/api.php
+++ routes/api.php
@@ -70,6 +70,7 @@ Route::middleware(['auth:sanctum'])->post('/typeform', function (Request $reques
   DB::setDefaultConnection('pgsql');
 
   //dd("request", $request->form_response["answers"]);
+  dd("request", $request->form_response["form_id"]);
 
   $m_formulario = MFormulario::updateOrCreate(
     ['ID_M_FORMULARIOS' => $request->form_id],
