fdcacaca89e5a514dfe64a79a7124913bae622fc
ivancho2802 <ivancho2802@hotmail.com>
2025-07-30 18:57:41 -0500

    se pasan datos erro

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -445,7 +445,7 @@
       </h4>
 
       <div class="mt-2 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-        <div class="grid grid-cols-1 md:grid-cols-2">
+        <div class="grid grid-cols-1 md:grid-cols-1">
           <div class="p-6">
             <div class="flex items-center">
               <div class="ml-4 text-lg leading-7 font-semibold">
