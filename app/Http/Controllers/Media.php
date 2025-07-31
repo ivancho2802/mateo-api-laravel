9763d290da423c4ceda22cb0d43aee2ac29860d0
ivancho2802 <ivancho2802@hotmail.com>
2025-07-31 02:31:13 -0500

    se pasan datos erro

--- app/Http/Controllers/Media.php
+++ app/Http/Controllers/Media.php
@@ -266,7 +266,7 @@ class Media extends Controller
             $arraycount = [$frase[1], 0];
           } else { */
             $conteo = count(MKoboRespuestas::where(DB::raw("'$preg'"), 'LIKE', DB::raw("CONCAT('%', \"VALOR\", '%')"))->get());
-            $totalResponses = count(MKoboRespuestas::get());
+            $totalResponses = count(MKoboRespuestas::distinct()->count('VALOR')->where(["REFERENCIA", "like", "%Correo%"])->get());
 
             if ($totalResponses > 0) {
                 $percentageOptionA = ($conteo / $totalResponses) * 100;
