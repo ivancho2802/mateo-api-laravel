0f64ab06dbd6075f3a4095d0d7c9fbc1ff839e9b
ivancho2802 <ivancho2802@hotmail.com>
2025-07-30 21:46:53 -0500

    se pasan datos erro

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -486,7 +486,7 @@
         <img src="{{ asset('images/constant_companion.png') }}" alt="Descripción de la imagen">
       </h4>
       
-      @foreach ($preguntas as $pregunta)
+      @foreach ($preguntas as  $key => $pregunta)
 
       <div class="mt-2 bg-white light:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
         <div class="grid grid-cols-1 md:grid-cols-1">
@@ -495,7 +495,7 @@
               <div class="ml-4 text-lg leading-7 font-semibold">
                 <a href="#"
                   class="underline text-gray-900 dark:text-white">
-                  {{collect($pregunta)->key()}} 
+                  {{$key}} 
                 </a>
               </div>
             </div>
