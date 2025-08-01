b3d3f4ed862d0cdd4058b3b993a88d67200e3480
IODIAZACF <150143784+IODIAZACF@users.noreply.github.com>
2025-07-31 20:52:07 -0500

    se ajusta

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -560,14 +560,6 @@
             type="button" @click="isOpen{{$loop->index}} = !isOpen{{$loop->index}}" type="button"
             aria-controls="filter-section-0" aria-expanded="false">
             <span class="font-medium text-light-900">{{$key2}}</span>
-            <!-- <span class="ml-6 flex items-center">
-          <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
-          </svg>
-          <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" d="M4 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H4.75A.75.75 0 014 10z" clip-rule="evenodd" />
-          </svg>
-          </span> -->
           </button>
           </h3>
 
@@ -640,15 +632,14 @@
           width: 630px;
           ">
             @foreach ($pregunt as $key3 => $preg)
-            <p>{{ $preg[1] }}</p>
+            <div class="col text-center"
+            style="     height: 80px;   align-items: center;    align-content: center; align-self: center;    text-align: center;">
             @if ($preg[1] == true)
-          <div class="col text-center"
-          style="     height: 80px;   align-items: center;    align-content: center; align-self: center;    text-align: center;">
           <div class="circle-creer bg-danger text-light circle-creer-{{round($preg[1])}} m-auto">
           {{round($preg[1])}}
           </div>
-          </div>
           @endif
+            </div>
           @endforeach
 
             </div>
