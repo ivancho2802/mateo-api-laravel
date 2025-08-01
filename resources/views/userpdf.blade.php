e24f9c185309f7bb1b0357cda18845df8c6200f4
IODIAZACF <150143784+IODIAZACF@users.noreply.github.com>
2025-08-01 06:11:03 -0500

    se ajusta

--- resources/views/userpdf.blade.php
+++ resources/views/userpdf.blade.php
@@ -470,7 +470,23 @@
 </head>
 
 <body class="antialiased">
+
+
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+    <!-- @if (Route::has('login'))
+    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+      @auth
+      <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+      @else
+      <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+
+      @if (Route::has('register'))
+      <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+      @endif
+      @endif
+    </div>
+    @endif -->
+
 
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
 
@@ -760,6 +776,7 @@
         Build v{{ Illuminate\Foundation\Application::VERSION }}
       </div>
     </div>
+  </div>
   </div>
 </body>
 
