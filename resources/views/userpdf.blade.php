78a002ba10a7569178532f0ef24d80b3ed90558e
IODIAZACF <150143784+IODIAZACF@users.noreply.github.com>
2025-08-01 06:45:35 -0500

    se ajusta

--- resources/views/userpdf.blade.php
+++ resources/views/userpdf.blade.php
@@ -6,12 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <title>Laravel</title>
-
-
-  @extends('layouts.user_type.guest')
-
-  @section('content')
-
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
