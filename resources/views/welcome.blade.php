99c3947e3248b5ed74e3ff336778456c02d47abd
IODIAZACF <150143784+IODIAZACF@users.noreply.github.com>
2025-07-31 20:20:16 -0500

    se ajusta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -597,17 +597,14 @@
           position: absolute;
           width: 630px;
           ">
-            @foreach ($pregunt as $key3 => $preg)
+          @foreach ($pregunt as $key3 => $preg)
             @if (round($preg[1]) > 0)
-          <div class="col text-center"
-          style="     height: 80px;   align-items: center;    align-content: center; align-self: center;    text-align: center;">
-          <div
-          class="circle-creer bg-danger text-light circle-creer-{{round($preg[1]) > 10 ? 10 : round($preg[1])}} m-auto">
-          {{round($preg[3])}}
-          </div>
-          </div>
-          @endif
-
+            <div class="col text-center p-0" style="     height: 80px;   align-items: center;    align-content: center; align-self: center;    text-align: center;">
+              <div class="circle-creer bg-danger text-light circle-creer-{{round($preg[1]) > 10 ? 10 : round($preg[1])}} m-auto">
+              {{round($preg[3])}}
+              </div>
+            </div>
+            @endif
           @endforeach
 
             </div>
