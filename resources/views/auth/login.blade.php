da6e23a79203d76092774ada78ed279ce483e215
IODIAZACF <150143784+IODIAZACF@users.noreply.github.com>
2026-08-03 16:14:23 -0500

    se ahbilita para probar si funcionara la solicitud de laravel con ajuste de url

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
         <div class="mt-2 bg-white light:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
 
-            <form method="POST" action="/session">
+            <form method="POST" action="/apidev/session">
                 @csrf
                 <!-- Email input -->
                 <div class="form-outline mb-4 mt-5">
