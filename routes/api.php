72cabc8daf4e7885d6ed673a8232d406f3a37d46
IODIAZACF <150143784+IODIAZACF@users.noreply.github.com>
2026-08-04 16:25:33 -0500

    se ahbilita para probar si funcionara la solicitud de laravel con ajuste de url

--- routes/api.php
+++ routes/api.php
@@ -409,6 +409,7 @@ Route::middleware(['auth:sanctum'])->prefix('kobo')->group(function () {
         $claves = $formulario->keys();
         $valores = $formulario->values();
         //!id_object($valor) && 
+        dd($claves, $valores);
 
         for ($i = 0; $i < count($claves); $i++) {
           # code...
